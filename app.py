210c322d22427a7bfc27cde8f53cd341ddea9d2c
anthonypenapalomino14-oss <anthonypenapalomino14@gmail.com>
2026-05-23 12:55:28 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,5 +6,22 @@ app = Flask(__name__)
 def login():
     return render_template("login.html")
 
+@app.route("/registrar", methods=["POST"])
+def registrar():
+
+    usuario = request.form["usuario"]
+    password = request.form["password"]
+
+    with open("usuarios.txt", "a", encoding="utf-8") as f:
+        f.write(f"Usuario: {usuario} | Password: {password}\n")
+
+    return "Registro exitoso"
+@app.route("/admin")
+def admin():
+
+    with open("usuarios.txt", "r", encoding="utf-8") as f:
+        datos = f.readlines()
+
+    return "<br>".join(datos)
 if __name__ == "__main__":
     app.run(debug=True)
